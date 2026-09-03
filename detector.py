d1fc046d4fa9115a1e96f0bcaa21e466905df1a6
Syed Areeb Abdullah <s.areebabdullah@gmail.com>
2026-09-03 01:50:02 -0500

    Extract source IP from packets
    Added functionality to print source IP address if packet has IP layer.

--- detector.py
+++ detector.py
@@ -1,7 +1,8 @@
 from scapy.all import sniff
 
 def process_packet(packet):
-    print(packet.summary())
+    if packet.haslayer("IP"):
+        print("Source IP:", packet["IP"].src)
 
 def main():
     print("Starting network monitor...")
